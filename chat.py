374eaac1fefd4b618a02dc473cb2934c258fa21c
Tanuj <Tanuj@cmu.edu>
2023-03-04 18:35:58 -0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,21 +1,22 @@
-# This file should be moved to your home directory! .../chat.py instead of .../Documents/TanTan/chatbot.py
+# This OPTIONAL file should be moved to your Home directory! .../chat.py instead of .../Documents/TanTan/chatbot.py
 # Allows running the chatbot in one simple line, with additional perks
 # Instead of executing the file as a script, it imports chat.py as a module, allowing for the user to remain in the Python environment
+# Permits arrow key functionality to see prior messages within an input() prompt, which can be an annoyance by default in the executable
+    # The above perk needs readline installed, using homebrew make things simpler.
 
+
+# Double check file is in ~/Users/{Name}/chat.py! and you are running this from {Name}! (Afaik, the default in terminal)
 # Check Chatbot README.md for how to run
 
 import sys
 # Add our repository to our path, so the chatbot can be imported correctly.
-sys.path.append('documents/tantan')
+
+sys.path.append('documents/tantan') # Make sure to tweak this path! This is just where I keep the repository on my machine.
 import chatbot
 
-#set args
-args = ['python_env']
 if __name__ == '__main__':
-    # Run the chatbot with the given args
-    # If you want to run the chatbot with different args, change the args variable above
-    # For example, to run the chatbot in debug mode, change args to ['python_env', '-d']
-    # For more info, see the README.md in the chatbot folder
-    args = ['python_env', 'config', 'davinci', '200']
-    
-chatbot.main_from_args(args)
+    chatbot.main_from_args(sys.argv)
+else:
+    # default configuration for chatbot when used as a module
+    args = ['python_env']
+    chatbot.main_from_args(args)
